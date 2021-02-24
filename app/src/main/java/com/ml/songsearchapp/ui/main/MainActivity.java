00b74320bd96e1d6df6962b72d0cccd4ad6b19c7
Matthew Levene <matthew.levene@hotmail.co.uk>
2021-02-24 16:03:55 +0000

    Move MainActivty class into a different package

---
+++ app/src/main/java/com/ml/songsearchapp/ui/main/MainActivity.java
@@ -0,0 +1,18 @@
+package com.ml.songsearchapp.ui.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ml.songsearchapp.BuildConfig;
+import com.ml.songsearchapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
