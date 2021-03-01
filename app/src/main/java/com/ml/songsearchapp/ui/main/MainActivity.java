5d7275babc3d2936e355c5f2742a450ce159d82e
Matthew Levene <matthew.levene@hotmail.co.uk>
2021-03-01 10:08:22 +0000

    Move MainActivity class and introduce NavFragmentHost to the layout

--- app/src/main/java/com/ml/songsearchapp/ui/main/MainActivity.java
+++ app/src/main/java/com/ml/songsearchapp/ui/main/MainActivity.java
@@ -1,18 +1,25 @@
 package com.ml.songsearchapp.ui.main;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
 import com.ml.songsearchapp.BuildConfig;
 import com.ml.songsearchapp.R;
+import com.ml.songsearchapp.ui.home.viewmodel.SearchSongViewModel;
 
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
 public class MainActivity extends AppCompatActivity {
+    private SearchSongViewModel vm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
 }
