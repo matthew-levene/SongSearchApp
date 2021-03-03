35faa33b35cba75e1f2440652427d44ca4b3ba31
Matthew Levene <matthew.levene@hotmail.co.uk>
2021-03-03 10:02:23 +0000

    Make class extend Serializable

--- app/src/main/java/com/ml/songsearchapp/db/entities/Image.java
+++ app/src/main/java/com/ml/songsearchapp/db/entities/Image.java
@@ -3,7 +3,9 @@ package com.ml.songsearchapp.db.entities;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-public class Image {
+import java.io.Serializable;
+
+public class Image implements Serializable {
 
     @SerializedName("#text")
     @Expose
