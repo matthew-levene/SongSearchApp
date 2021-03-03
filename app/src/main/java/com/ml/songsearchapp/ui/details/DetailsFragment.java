8f558715dbd80dfd22b63eada3635a39c95e3fca
Matthew Levene <matthew.levene@hotmail.co.uk>
2021-03-03 14:34:28 +0000

    Implement UI logic for Details/Similar Songs fragment

---
+++ app/src/main/java/com/ml/songsearchapp/ui/details/DetailsFragment.java
@@ -0,0 +1,111 @@
+package com.ml.songsearchapp.ui.details;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.browser.customtabs.CustomTabsIntent;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.ml.songsearchapp.R;
+import com.ml.songsearchapp.databinding.FragmentDetailsBinding;
+import com.ml.songsearchapp.db.entities.Song;
+import com.ml.songsearchapp.db.entities.similar.SimilarSong;
+import com.ml.songsearchapp.ui.details.adapter.SimilarSearchAdapter;
+import com.ml.songsearchapp.ui.details.viewmodel.SimilarSongsViewModel;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
+public class DetailsFragment extends Fragment implements SimilarSearchAdapter.SearchAdapterDelegate {
+    private FragmentDetailsBinding binding;
+    private SimilarSearchAdapter similarSearchAdapter;
+    SimilarSongsViewModel viewModel;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentDetailsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        viewModel = new ViewModelProvider(this).get(SimilarSongsViewModel.class);
+        setupViews();
+        setupObserver();
+
+    }
+
+    private void setupViews() {
+        Song song = (Song) requireArguments().getSerializable("songArgs");
+        SimilarSong similarSong = (SimilarSong) requireArguments().getSerializable("similarSongArgs");
+
+        if(song != null) {
+            viewModel.retrieveSimilarSongs(song.getName(), song.getArtist());
+
+            binding.artistNameTextview.setText(song.getArtist());
+            binding.songTitleTextview.setText(song.getName());
+            binding.listenerCountTextview.setText(getString(R.string.x_listeners_text, song.getListeners()));
+            binding.goToStreamButton.setOnClickListener(v -> openStream(song.getUrl()));
+
+        } else {
+            viewModel.retrieveSimilarSongs(similarSong.getName(), similarSong.getArtist().getName());
+
+            binding.artistNameTextview.setText(similarSong.getArtist().getName());
+            binding.songTitleTextview.setText(similarSong.getName());
+            binding.goToStreamButton.setOnClickListener(v -> openStream(similarSong.getUrl()));
+
+            binding.listenerCountTextview.setText(getString(R.string.x_times_played_text, similarSong.getPlaycount()));
+            binding.listenerCountImageview.setImageResource(R.drawable.ic_play);
+        }
+
+        Glide.with(requireView())
+                .load(R.drawable.ic_music_note)
+                .into(binding.songIconImageview);
+
+        similarSearchAdapter = new SimilarSearchAdapter(this);
+        binding.similiarSongsRecycler.setAdapter(similarSearchAdapter);
+    }
+
+    private void setupObserver() {
+        viewModel.songMatchesLiveData.observe(getViewLifecycleOwner(), songMatches -> {
+            similarSearchAdapter.submitList(songMatches.track);
+        });
+    }
+
+    private void openStream(String url) {
+        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+        CustomTabsIntent customTabsIntent = builder.build();
+        customTabsIntent.launchUrl(requireContext(), Uri.parse(url));
+    }
+
+    @Override
+    public void songClick(SimilarSong song) {
+        reloadFragment(song);
+    }
+
+    private void reloadFragment(SimilarSong song) {
+        DetailsFragment dt = this;
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("songArgs", null);
+        bundle.putSerializable("similarSongArgs", song);
+        dt.setArguments(bundle);
+
+        getParentFragmentManager()
+                .beginTransaction()
+                .remove(this)
+                .add(dt, getTag())
+                .commit();
+    }
+}
---
+++ app/src/main/java/com/ml/songsearchapp/ui/details/adapter/SimilarSearchAdapter.java
@@ -0,0 +1,82 @@
+package com.ml.songsearchapp.ui.details.adapter;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.ml.songsearchapp.R;
+import com.ml.songsearchapp.databinding.ItemGridSongBinding;
+import com.ml.songsearchapp.databinding.ItemListSongBinding;
+import com.ml.songsearchapp.db.entities.Song;
+import com.ml.songsearchapp.db.entities.SongMatches;
+import com.ml.songsearchapp.db.entities.similar.SimilarSong;
+
+
+public class SimilarSearchAdapter extends ListAdapter<SimilarSong, SimilarSearchAdapter.SongViewHolder> {
+
+    public interface SearchAdapterDelegate {
+        void songClick(SimilarSong song);
+    }
+
+    SearchAdapterDelegate adapterDelegate;
+
+    public SimilarSearchAdapter(SearchAdapterDelegate adapterDelegate) {
+        super(DIFF_CALLBACK);
+        this.adapterDelegate = adapterDelegate;
+    }
+
+    @NonNull
+    @Override
+    public SongViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ItemGridSongBinding binding = ItemGridSongBinding.inflate(
+                LayoutInflater.from(parent.getContext()),
+                parent,
+                false
+        );
+        return new SongViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SongViewHolder holder, int position) {
+        SimilarSong song = getItem(position);
+        holder.songTitleTextView.setText(song.getName());
+        holder.artistNameTextView.setText(song.getArtist().getName());
+        Glide.with(holder.itemView)
+                .load(R.drawable.ic_music_note)
+                .into(holder.songImageView);
+
+        holder.itemView.setOnClickListener(v -> adapterDelegate.songClick(song));
+    }
+
+    public static class SongViewHolder extends RecyclerView.ViewHolder {
+        ImageView songImageView;
+        TextView artistNameTextView, songTitleTextView;
+
+        public SongViewHolder(ItemGridSongBinding binding) {
+            super(binding.getRoot());
+            artistNameTextView = binding.artistNameTextview;
+            songTitleTextView = binding.songTitleTextview;
+            songImageView = binding.songIconImageview;
+        }
+    }
+
+    private final static DiffUtil.ItemCallback<SimilarSong> DIFF_CALLBACK =
+            new DiffUtil.ItemCallback<SimilarSong>() {
+                @Override
+                public boolean areItemsTheSame(@NonNull SimilarSong oldItem, @NonNull SimilarSong newItem) {
+                    return oldItem == newItem;
+                }
+
+                @Override
+                public boolean areContentsTheSame(@NonNull SimilarSong oldItem, @NonNull SimilarSong newItem) {
+                    return oldItem.getName().equals(newItem.getName());
+                }
+            };
+}
